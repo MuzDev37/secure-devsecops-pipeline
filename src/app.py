7e388a53f059f38deb1819c34ac605474d6b8adb
MuzDev37 <muzdev0@gmail.com>
2026-07-05 17:20:31 +0000

    fitur: menambahkan aplikasi flask awal dengan celah keamanan

--- src/app.py
+++ src/app.py
@@ -1,20 +1,23 @@
 from flask import Flask, request
-import os
+import werkzeug
 
 app = Flask(__name__)
 
-# TARGET GITLEAKS: Token rahasia buatan yang menyerupai AWS Key asli
-AWS_SECRET_KEY = "AKIAIOSFODNN7EXAMPLE-FAKE-KEY-DO-NOT-USE"
+# 1. Pemicu Gitleaks (Secret Leakage / Information Disclosure)
+AWS_SECRET_KEY = "AKIAIMZAMB1AEXAMPLE1234567890abcdefghij"
+DB_PASSWORD = "super_secret_password_123"
 
 @app.route('/')
 def home():
-    return "<h1>DevSecOps Secure Pipeline Demo</h1>"
+    return "<h1>DevSecOps Pipeline - VULNERABLE KONDISI BEFORE</h1>"
 
 @app.route('/execute')
 def execute():
-    # TARGET BANDIT: Menggunakan fungsi eval() yang sangat rawan eksploitasi
+    # 2. Pemicu Bandit (Malicious Commit / Code Injection / Tampering)
+    # Menggunakan fungsi eval() dari input query parameter secara buta
     cmd = request.args.get('cmd', 'print("Hello")')
-    return f"Hasil Eksekusi: {eval(cmd)}"
+    hasil = eval(cmd) 
+    return f"Hasil Eksekusi: {hasil}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
